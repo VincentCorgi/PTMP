7d796b47abc9a3da6ef03e776718fe9c2633d57a
VincentCorgi <a325698741z@gmail.com>
2022-05-10 20:49:45 +0800

    fix: 調用合約方法 send跟call 待優化

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,7 +5,7 @@
       <button @click="addTender">新增招標</button>
       <button @click="logout">登出</button>
       <button @click="searchETH">查詢錢包ETH餘額</button>
-      <button @click="searchToken">查詢代幣餘額</button>
+      <button @click="test">測試鈕</button>
     </div>
     <div v-show="$route.name === 'AddTender'">
       <button @click="save">確認</button>
@@ -15,9 +15,17 @@
 </template>
 
 <script>
+import { BIconWindowSidebar, LayoutPlugin } from 'bootstrap-vue'
 import Web3 from 'web3'
+import Web3Utils from 'web3-utils'
 export default {
   name: 'Navbar',
+  data () {
+    return {
+      address: '',
+      name: ''
+    }
+  },
   methods: {
     searchETH () {
       const web3 = new Web3(window.web3.currentProvider)
@@ -26,23 +34,113 @@ export default {
       console.log(fromAddress)
       web3.eth.getBalance(fromAddress, (err, res) => {
         if (!err) {
+          console.log(res)
+          console.log(res.toNumber())
           console.log('ETH餘額==', res.toNumber() / Math.pow(10, 18))
+          // alert(`ETH餘額=${res.toNumber() / Math.pow(10, 18)}`)
         }
       })
     },
-    searchToken () {
+    async test () {
+      // 建立web3
       const web3 = new Web3(window.web3.currentProvider)
+      web3.eth.getBlockNumber().then(console.log)
+      // 拿取當前地址
       const fromAddress = web3.eth.accounts[0]
-      const ethContract = web3.eth.contract('代幣合約Abi')
-      console.log(ethContract)
-      const functionInstance = ethContract.at('代幣合約地址')
-      console.log(functionInstance)
-      functionInstance.balanceOf(fromAddress, (err, res) => {
-        if (!err) {
-          // 代幣精度根據所發行的代幣合約精度換算
-          console.log('代幣餘額==', res.toNumber() / Math.pow(10, 18))
+      this.address = fromAddress
+      // 合約地址
+      const contractAddress = '0xFE0da3c22a75266cC00B792d0C3A4C678d6390bb'
+      // 合約abi
+      const contractABI = [
+        {
+          inputs: [],
+          name: 'lookupTenderName',
+          outputs: [
+            {
+              internalType: 'string',
+              name: '',
+              type: 'string'
+            }
+          ],
+          stateMutability: 'view',
+          type: 'function'
+        },
+        {
+          inputs: [],
+          name: 'retrieve',
+          outputs: [
+            {
+              internalType: 'uint256',
+              name: '',
+              type: 'uint256'
+            }
+          ],
+          stateMutability: 'view',
+          type: 'function'
+        },
+        {
+          inputs: [
+            {
+              internalType: 'uint256',
+              name: 'num',
+              type: 'uint256'
+            }
+          ],
+          name: 'store',
+          outputs: [],
+          stateMutability: 'nonpayable',
+          type: 'function'
+        },
+        {
+          inputs: [
+            {
+              internalType: 'string',
+              name: 'str',
+              type: 'string'
+            }
+          ],
+          name: 'writeTenderName',
+          outputs: [],
+          stateMutability: 'nonpayable',
+          type: 'function'
         }
-      })
+      ]
+
+      // 連接合約
+      const ethContract = web3.eth.contract(contractABI).at(contractAddress)
+
+      // 使用sendTransaction()
+      // const sendData = ethContract.writeTenderName.getData('星空')
+      // web3.eth.sendTransaction(
+      //   {
+      //     from: fromAddress,
+      //     to: contractAddress,
+      //     data: sendData
+      //   },
+      //   function (err, hash) {
+      //     if (!err) {
+      //       console.log(hash)
+      //     }
+      //   }
+      // )
+
+      // 使用call
+      const callData = ethContract.lookupTenderName.getData()
+      // const hash = function (err, hash) {
+      //   if (!err) {
+      //     console.log(hash)
+      //     console.log(Web3Utils.hexToUtf8(hash))
+      //     this.name = Web3Utils.hexToUtf8(hash)
+      //     return Web3Utils.hexToUtf8(hash)
+      //   }
+      // }
+      // const aaa = web3.eth.call(
+      //   {
+      //     to: contractAddress,
+      //     data: callData
+      //   },
+      //   hash
+      // )
     },
     addTender () {
       this.$router.push({ name: 'AddTender' })
