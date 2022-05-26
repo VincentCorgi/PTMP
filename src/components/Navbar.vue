0cdc15f1a63d261506f52a8a8e26fedbfd77610d
VincentCorgi <a325698741z@gmail.com>
2022-05-27 00:32:44 +0800

    fix: 透過getCount和tenders拿到全部的標案

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,25 +2,22 @@
   <div class="navbar">
     ip: 163.13.138.226
     <span style="color: white">Navbar</span>
+    <span style="color: white">{{ name }}</span>
     <div v-show="$route.name === 'Dashboard'">
       <button @click="addTender">新增招標</button>
       <button @click="logout">登出</button>
-      <button @click="searchETH">查詢錢包ETH餘額</button>
-      <button @click="test">測試鈕</button>
+      <b-button variant="danger" @click="test">測試鈕</b-button>
+
     </div>
     <div v-show="$route.name === 'AddTender'">
       <button @click="save">確認</button>
       <button @click="back">返回</button>
-      <p>this is master</p>
-      <p>git change branch</p>
-      <p>git ip 比對測試</p>
     </div>
   </div>
 </template>
 
 <script>
 import Web3 from 'web3'
-import Web3Utils from 'web3-utils'
 export default {
   name: 'Navbar',
   data () {
@@ -29,29 +26,24 @@ export default {
       name: ''
     }
   },
+  // computed: {
+  //   ...mapState({
+  //     address: state => state.address
+  //   })
+  // },
+  // mounted () {
+  //   console.log(this.address)
+  // },
   methods: {
-    searchETH () {
-      const web3 = new Web3(window.web3.currentProvider)
-      console.log(web3)
-      const fromAddress = web3.eth.accounts[0]
-      console.log(fromAddress)
-      web3.eth.getBalance(fromAddress, (err, res) => {
-        if (!err) {
-          console.log(res)
-          console.log(res.toNumber())
-          console.log('ETH餘額==', res.toNumber() / Math.pow(10, 18))
-          // alert(`ETH餘額=${res.toNumber() / Math.pow(10, 18)}`)
-        }
-      })
-    },
     async test () {
       // 建立web3
-      const web3 = new Web3(window.web3.currentProvider)
+      const web3 = new Web3(Web3.givenProvider || 'ws://http://192.168.12.220:8080')
       // 拿取當前地址
+      // console.log(web3.eth.getAccounts(console.log))
       const fromAddress = web3.eth.accounts[0]
       this.address = fromAddress
       // 合約地址
-      const contractAddress = '0xF54dB4adc2267654Cad98228e0194a40573e2430'
+      const contractAddress = '0x88493AFD632242304922B140E6247E44A24e19EF'
       // 合約abi
       const contractABI = [
         {
@@ -70,6 +62,19 @@ export default {
           name: 'addTender',
           outputs: [],
           stateMutability: 'nonpayable',
+          type: 'function'
+        },
+        {
+          inputs: [],
+          name: 'getCount',
+          outputs: [
+            {
+              internalType: 'uint256',
+              name: '',
+              type: 'uint256'
+            }
+          ],
+          stateMutability: 'view',
           type: 'function'
         },
         {
@@ -96,7 +101,7 @@ export default {
           stateMutability: 'view',
           type: 'function'
         },
-        {
+        { // 在tenders拿值
           inputs: [
             {
               internalType: 'uint256',
@@ -121,42 +126,56 @@ export default {
           type: 'function'
         }
       ]
-
       // 連接合約
-      const ethContract = web3.eth.contract(contractABI).at(contractAddress)
+      const ethContract = new web3.eth.Contract(contractABI, contractAddress)
 
-      // 使用sendTransaction()
-      // const sendData = ethContract.addTender.getData('範例一採購', 'aa')
-      // console.log(sendData)
-      // web3.eth.sendTransaction(
-      //   {
-      //     from: fromAddress,
-      //     to: contractAddress,
-      //     data: sendData
-      //   },
-      //   function (err, hash) {
-      //     if (!err) {
-      //       console.log(hash)
+      // ethContract.methods
+      //   .addTender('範例二', '對')
+      //   .send(
+      //     {
+      //       from: '0x904e7C77c7D5ed71A19eF218e7962CF074aB8346'
       //     }
-      //   }
-      // )
-
-      // .on('err', function (err) {})
-
-      // 使用call
-      const callData = ethContract.tenders.getData(0)
-      web3.eth.call(
-        {
-          to: contractAddress,
-          data: callData
-        },
-        function (err, hash) {
-          if (!err) {
-            console.log(hash)
-            console.log(Web3Utils.hexToUtf8(hash))
+      //   )
+      //   .then(function (receipt) {
+      //     console.log(receipt)
+      //   })
+      const list = []
+      const amount = await ethContract.methods
+        .getCount()
+        .call(
+          {
+            from: '0x904e7C77c7D5ed71A19eF218e7962CF074aB8346'
           }
-        }
-      )
+        )
+        .then(function (receipt) {
+          return receipt
+        })
+      for (let i = 0; i < 2; i++) {
+        const tender = await ethContract.methods
+          .tenders(i)
+          .call(
+            {
+              from: '0x904e7C77c7D5ed71A19eF218e7962CF074aB8346'
+            }
+          )
+          .then(function (receipt) {
+            return receipt
+          })
+        list.push(tender)
+      }
+      this.name = list.map(item => {
+        return { name: item.name, subjectProcurement: item.subjectProcurement }
+      })
+      // const aaa = await ethContract.methods
+      //   .tenders(0)
+      //   .call(
+      //     {
+      //       from: '0x904e7C77c7D5ed71A19eF218e7962CF074aB8346'
+      //     }
+      //   )
+      //   .then(function (receipt) {
+      //     return receipt
+      //   })
     },
     addTender () {
       this.$router.push({ name: 'AddTender' })
