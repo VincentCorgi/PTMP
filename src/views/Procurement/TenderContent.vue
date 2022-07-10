ca6421bcb627bf3fba4d31d9d233bab5192dfacf
VincentCorgi <a325698741z@gmail.com>
2022-07-10 14:50:35 +0800

    fix: 查看招標資訊完成頁面（剩下投標後的顯示）

--- src/views/Procurement/TenderContent.vue
+++ src/views/Procurement/TenderContent.vue
@@ -2,9 +2,10 @@
   <b-container fluid>
     <!-- class="vh-100" align-v="center" -->
     <b-row align-h="center">
+      <b-col cols="3"></b-col>
       <b-col cols="6">
       <div class="superiorEntity">
-        <div style="font-size: 20px; font-weight: bold; margin-top: 0px; margin-bottom: 10px;">機關資訊</div>
+        <div style="font-size: 20px; font-weight: bold; margin-bottom: 10px;">機關資訊</div>
         <b-form-group
             label-cols-sm="4"
             label-cols-lg="8"
@@ -52,63 +53,107 @@
           </b-form-group>
       </div>
       </b-col>
-    </b-row>
-    <!-- --------------------------------------------------------------- -->
-    <b-row align-h="center">
-      <b-col class="">
-        <b-row align-h="center">
-          <b-col cols="2">
-            <span>標案名稱：</span>
-          </b-col>
-          <b-col cols="4">
-            <div v-if="isSave">
-              <span>{{ currentTender.name }}</span>
-            </div>
-            <div v-else>
-              <b-form-input
-                v-model="currentTender.name"
-              />
-            </div>
-          </b-col>
-        </b-row>
-        <b-row align-h="center">
-          <b-col cols="2">
-            <span>標的分類：</span>
-          </b-col>
-          <b-col cols="4">
-            <div v-if="isSave">
-              <span>{{ currentTender.subjectProcurement }}</span>
-            </div>
-            <div v-else>
-              <b-form-input
-                v-model="currentTender.subjectProcurement"
-              />
-            </div>
-          </b-col>
-        </b-row>
+      <b-col cols="3">
+        <div style="margin: 50px 0px 0px 50px">
+          <b-icon
+            icon="arrow-right"
+            animation="cylon"
+            font-scale="4"
+            style="margin: 20px 10px 0px 0px"
+          />
+          <b-button
+            size="lg"
+            variant="info"
+            style="margin: 0px 0px 20px 10px"
+            @click="biddingTender"
+          >去投標</b-button>
+        </div>
       </b-col>
+    </b-row>
+    <!-- ------------------------------------------------------------------------------------------ -->
+    <b-row align-h="center">
+      <b-col></b-col>
+      <b-col cols="6">
+        <div class="tenderInfo">
+          <div style="font-size: 20px; font-weight: bold; margin-bottom: 10px;">標案資訊</div>
+          <b-form-group
+            label-cols-sm="3"
+            label-align-sm="left"
+            style="padding-left: 20px; margin-bottom: 12px"
+            label="標案名稱："
+          >
+            <span style="float: left;">{{ currentTender.name }}</span>
+          </b-form-group>
+          <b-form-group
+            label-cols-sm="3"
+            label-align-sm="left"
+            style="padding-left: 20px; margin-bottom: 12px"
+            label="招標方式："
+          >
+            <span style="float: left;">{{ currentTender.tenderMethod }}</span>
+          </b-form-group>
+          <b-form-group
+            label-cols-sm="3"
+            label-align-sm="left"
+            style="padding-left: 20px; margin-bottom: 12px"
+            label="採購性質："
+          >
+            <span style="float: left;">{{ currentTender.procurementProperty }}</span>
+          </b-form-group>
+          <b-form-group
+            label-cols-sm="3"
+            label-align-sm="left"
+            style="padding-left: 20px; margin-bottom: 12px"
+            label="公告日："
+          >
+            <span style="float: left;">{{ currentTender.publishingDate }}</span>
+          </b-form-group>
+          <b-form-group
+            label-cols-sm="3"
+            label-align-sm="left"
+            style="padding-left: 20px; margin-bottom: 12px"
+            label="預算金額："
+          >
+            <span style="float: left;">{{ currentTender.budgetAmount }}</span>
+          </b-form-group>
+          <b-form-group
+            label-cols-sm="3"
+            label-align-sm="left"
+            style="padding-left: 20px; margin-bottom: 12px"
+            label="截止投標："
+          >
+            <span style="float: left;">{{ currentTender.biddingDeadline }}</span>
+          </b-form-group>
+          <b-form-group
+            label-cols-sm="3"
+            label-align-sm="left"
+            style="padding-left: 20px; margin-bottom: 12px"
+            label="開標日期："
+          >
+            <span style="float: left;">{{ currentTender.openingDate }}</span>
+          </b-form-group>
+        </div>
+      </b-col>
+      <b-col></b-col>
     </b-row>
   </b-container>
 </template>
 
 <script>
-import { mapState, mapMutations } from 'vuex'
+import { mapState } from 'vuex'
+
 export default {
   name: 'TenderContent',
   computed: {
     ...mapState({
       currentTender: state => state.tender.current,
-      currentFirm: state => state.firm.current,
-      isSave: state => state.tender.isSave
+      currentFirm: state => state.firm.current
     })
   },
-  async mounted () {
-    (!this.currentTender.name)
-      ? this.setIsSave(false)
-      : this.setIsSave(true)
-  },
   methods: {
-    ...mapMutations('tender', ['setIsSave'])
+    biddingTender () {
+
+    }
   }
 }
 </script>
@@ -116,7 +161,12 @@ export default {
 <style>
 .superiorEntity{
   margin-top: 10px;
-  background-color: rgb(116, 176, 235);
+  background-color: rgb(190, 218, 247);
+  border-radius: 25px;
+}
+.tenderInfo{
+  margin-top: 20px;
+  background-color: rgb(243, 229, 200);
   border-radius: 25px;
 }
 </style>
