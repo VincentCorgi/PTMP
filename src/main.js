55ca4df93f33aebe165b8bbc0c0cd1aaf251cecb
VincentCorgi <a325698741z@gmail.com>
2022-07-10 14:19:52 +0800

    fix: 新增use bootstrap icon

--- src/main.js
+++ src/main.js
@@ -4,12 +4,14 @@ import Web3 from 'web3'
 import Vuex from 'vuex'
 import store from './store'
 import router from './router'
-import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap-vue/dist/bootstrap-vue-icons.min.css'
+import { BootstrapVueIcons, BootstrapVue } from 'bootstrap-vue'
 
 Vue.use(Vuex)
 Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
 Vue.prototype.Web3 = Web3
 Vue.config.productionTip = false
 
