1456733231adc7b799a986213d053eec9f75afbb
VincentCorgi <a325698741z@gmail.com>
2022-07-13 04:54:36 +0800

    fix: 當前合約堪用

--- src/service/index.js
+++ src/service/index.js
@@ -165,10 +165,18 @@ import Web3 from 'web3'
 
 const web3 = new Web3(Web3.givenProvider || 'ws://http://192.168.12.220:8080')
 // 合約地址
-const contractAddress = '0x88493AFD632242304922B140E6247E44A24e19EF'
+const contractAddress = '0xCdd60e642808AaBB9032d721810A364a1327Ad85'
 // 合約abi
+// 新增廠商 addManufacturer
+// 新增標案 addTender
+// 新增投標 addTenderBidder
+// 標案數量 amountTender
+// 取得投標商地址 getBiddersAddress
+// 投標查詢（單筆） lookupBidder
+// 廠商列表 manufacturers
+// 標案列表 tenders
 const contractABI = [
-  {
+  { // 新增廠商 addManufacturer
     inputs: [
       {
         internalType: 'string',
@@ -177,7 +185,65 @@ const contractABI = [
       },
       {
         internalType: 'string',
-        name: 'subjectProcurement',
+        name: 'email',
+        type: 'string'
+      },
+      {
+        internalType: 'string',
+        name: 'contact',
+        type: 'string'
+      },
+      {
+        internalType: 'string',
+        name: 'contactNumber',
+        type: 'string'
+      },
+      {
+        internalType: 'string',
+        name: 'contactAddress',
+        type: 'string'
+      }
+    ],
+    name: 'addManufacturer',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function'
+  },
+  { // 新增標案 addTender
+    inputs: [
+      {
+        internalType: 'string',
+        name: 'name',
+        type: 'string'
+      },
+      {
+        internalType: 'uint256',
+        name: 'procurementProperty',
+        type: 'uint256'
+      },
+      {
+        internalType: 'uint256',
+        name: 'tenderMethod',
+        type: 'uint256'
+      },
+      {
+        internalType: 'string',
+        name: 'publishingDate',
+        type: 'string'
+      },
+      {
+        internalType: 'uint256',
+        name: 'budgetAmount',
+        type: 'uint256'
+      },
+      {
+        internalType: 'string',
+        name: 'biddingDeadline',
+        type: 'string'
+      },
+      {
+        internalType: 'string',
+        name: 'openingDate',
         type: 'string'
       }
     ],
@@ -186,9 +252,37 @@ const contractABI = [
     stateMutability: 'nonpayable',
     type: 'function'
   },
-  {
+  { // 新增投標 addTenderBidder
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: 'tenderId',
+        type: 'uint256'
+      },
+      {
+        internalType: 'uint256',
+        name: 'price',
+        type: 'uint256'
+      },
+      {
+        internalType: 'string',
+        name: 'exerciseDate',
+        type: 'string'
+      },
+      {
+        internalType: 'bool',
+        name: 'isSME',
+        type: 'bool'
+      }
+    ],
+    name: 'addTenderBidder',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function'
+  },
+  { // 標案數量 amountTender
     inputs: [],
-    name: 'getCount',
+    name: 'amountTender',
     outputs: [
       {
         internalType: 'uint256',
@@ -199,7 +293,65 @@ const contractABI = [
     stateMutability: 'view',
     type: 'function'
   },
-  {
+  { // 取得投標商地址 getBiddersAddress
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: 'tenderId',
+        type: 'uint256'
+      }
+    ],
+    name: 'getBiddersAddress',
+    outputs: [
+      {
+        internalType: 'address[]',
+        name: '',
+        type: 'address[]'
+      }
+    ],
+    stateMutability: 'view',
+    type: 'function'
+  },
+  { // 投標查詢（單筆） lookupBidder
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: 'tenderId',
+        type: 'uint256'
+      },
+      {
+        internalType: 'address',
+        name: 'addr',
+        type: 'address'
+      }
+    ],
+    name: 'lookupBidder',
+    outputs: [
+      {
+        internalType: 'address',
+        name: '',
+        type: 'address'
+      },
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256'
+      },
+      {
+        internalType: 'string',
+        name: '',
+        type: 'string'
+      },
+      {
+        internalType: 'bool',
+        name: '',
+        type: 'bool'
+      }
+    ],
+    stateMutability: 'view',
+    type: 'function'
+  },
+  { // 廠商列表 manufacturers
     inputs: [
       {
         internalType: 'address',
@@ -207,8 +359,13 @@ const contractABI = [
         type: 'address'
       }
     ],
-    name: 'tenderList',
+    name: 'manufacturers',
     outputs: [
+      {
+        internalType: 'address',
+        name: 'addr',
+        type: 'address'
+      },
       {
         internalType: 'string',
         name: 'name',
@@ -216,14 +373,29 @@ const contractABI = [
       },
       {
         internalType: 'string',
-        name: 'subjectProcurement',
+        name: 'contact',
+        type: 'string'
+      },
+      {
+        internalType: 'string',
+        name: 'contactNumber',
+        type: 'string'
+      },
+      {
+        internalType: 'string',
+        name: 'contactAddress',
+        type: 'string'
+      },
+      {
+        internalType: 'string',
+        name: 'email',
         type: 'string'
       }
     ],
     stateMutability: 'view',
     type: 'function'
   },
-  { // 在tenders拿值
+  { // 標案列表 tenders
     inputs: [
       {
         internalType: 'uint256',
@@ -234,13 +406,48 @@ const contractABI = [
     name: 'tenders',
     outputs: [
       {
+        internalType: 'address',
+        name: 'addr',
+        type: 'address'
+      },
+      {
+        internalType: 'uint256',
+        name: 'id',
+        type: 'uint256'
+      },
+      {
         internalType: 'string',
         name: 'name',
         type: 'string'
       },
       {
+        internalType: 'uint256',
+        name: 'procurementProperty',
+        type: 'uint256'
+      },
+      {
+        internalType: 'uint256',
+        name: 'tenderMethod',
+        type: 'uint256'
+      },
+      {
         internalType: 'string',
-        name: 'subjectProcurement',
+        name: 'publishingDate',
+        type: 'string'
+      },
+      {
+        internalType: 'uint256',
+        name: 'budgetAmount',
+        type: 'uint256'
+      },
+      {
+        internalType: 'string',
+        name: 'biddingDeadline',
+        type: 'string'
+      },
+      {
+        internalType: 'string',
+        name: 'openingDate',
         type: 'string'
       }
     ],
