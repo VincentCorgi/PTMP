65f256b5a3daa20d2d12d1f3ff3a958c514f2479
VincentCorgi <a325698741z@gmail.com>
2022-07-14 21:22:17 +0800

    fix: 移動文字跟登入鈕位置

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,8 +2,8 @@
   <b-container fluid>
     <b-row class="vh-100" align-v="center">
       <b-col>
-        <div style="font-size: 50px; margin: 0px 10px 100px 0px">營造廠採購交易管理平台</div>
-        <b-button size="lg" variant="success" @click="signIn">連接MetaMask錢包並登入</b-button>
+        <div style="font-size: 50px; margin: 0px 500px 100px 0px;">營造廠採購交易管理平台</div>
+        <b-button size="lg" variant="success" @click="signIn" style="margin-right: 500px">連接MetaMask錢包並登入</b-button>
       </b-col>
     </b-row>
   </b-container>
